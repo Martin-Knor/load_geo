4ce618a12a334b989950e64d486bbe9f8de19f5b
Martin-Knor <m.knor@djhec.sk>
2024-06-25 15:37:36 +0200

    feat: re-typing GeoJson data into geo-types datatypes

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-trying to load geojson file in rust
+showcase of loading data from .geojson file using "geojson" crate and re-typing this data into "geo-types" types used by the "geo" crate
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 use std::fs::File;
 use std::io::Read;
-use geojson::GeoJson;
+use geo_types::{Geometry, GeometryCollection};
+use geojson::{quick_collection, GeoJson};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Open the GeoJSON file
     let mut file = File::open("src/test.geojson")?;
     
     // Read the file contents into a string
-    let mut data = String::new();
-    file.read_to_string(&mut data)?;
+    let mut geojson_string = String::new();
+    file.read_to_string(&mut geojson_string)?;
     
     // Parse the GeoJSON string
-    let geojson = data.parse::<GeoJson>()?;
+    let geojson = geojson_string.parse::<GeoJson>()?;
+    
+    // transform contents of GeoJason into types supported by geo-types crtate
+    // https://docs.rs/geojson/0.22.4/geojson/index.html#use-geojson-with-other-crates-by-converting-to-geo-types
+    let collection: GeometryCollection<f64> = quick_collection(&geojson).unwrap();
     
     // Process the GeoJSON data
     match geojson {
@@ -29,6 +34,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         GeoJson::Geometry(geometry) => {
             println!("{:?}", geometry);
         },
+    }
+    
+    
+    println!("\n\n");
+
+
+    // Print the contents of the GeometryCollection
+    for geometry in &collection {
+        match geometry {
+            Geometry::Point(point) => println!("Point: {:?}", point),
+            Geometry::Polygon(polygon) => println!("Polygon: {:?}", polygon),
+            Geometry::MultiPolygon(multi_polygon) => println!("MultiPolygon: {:?}", multi_polygon),
+            Geometry::GeometryCollection(geometry_collection) => {
+                println!("GeometryCollection: {:?}", geometry_collection)
+            },
+            _ => return Err("Unsupported geometry type found".into()),
+        }
     }
 
     Ok(())
