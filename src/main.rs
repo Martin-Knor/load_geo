bea8a26a723cab837acd6b0de8ff46728c9c3978
Martin-Knor <m.knor@djhec.sk>
2024-06-25 11:57:09 +0200

    feat: program can load .geojson file and parse it into features

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::Read;
+use geojson::GeoJson;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Open the GeoJSON file
+    let mut file = File::open("src/test.geojson")?;
+    
+    // Read the file contents into a string
+    let mut data = String::new();
+    file.read_to_string(&mut data)?;
+    
+    // Parse the GeoJSON string
+    let geojson = data.parse::<GeoJson>()?;
+    
+    // Process the GeoJSON data
+    match geojson {
+        //if geojson is FeatureCollection type loop over its features
+        GeoJson::FeatureCollection(collection) => {
+            for feature in collection.features {
+                println!("{:?}", feature);
+            }
+        },
+        //if geojson is Feature type 
+        GeoJson::Feature(feature) => {
+            println!("{:?}", feature);
+        },
+        //if geojson is Geometry type 
+        GeoJson::Geometry(geometry) => {
+            println!("{:?}", geometry);
+        },
+    }
+
+    Ok(())
 }
